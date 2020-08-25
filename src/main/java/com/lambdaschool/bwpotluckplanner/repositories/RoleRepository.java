33a49af241be8e9ad5ca538357859805160b1c0a
Shanon Fritz <64600578+sfritz24@users.noreply.github.com>
2020-08-24 20:58:57 -0500

    Delete RoleRepository.java

--- src/main/java/com/lambdaschool/bwpotluckplanner/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lambdaschool.bwpotluckplanner.repositories;
-
-import com.lambdaschool.bwpotluckplanner.models.Role;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RoleRepository extends CrudRepository<Role, Long>
-{
-    Role findByNameIgnoreCase(String name);
-}
